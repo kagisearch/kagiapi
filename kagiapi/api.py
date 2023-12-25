ce440aaff48fcdc67f008be0c63b14f01404bc6a
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2023-12-25 18:39:26 +0300

    returning TypedDict to make coding easier

--- kagiapi/api.py
+++ kagiapi/api.py
@@ -1,27 +1,31 @@
 import os
 from typing import Optional, Union, Dict
-
 import requests
+from kagiapi.models import SearchResponse
 
 
 class KagiClient:
-    BASE_URL = "https://kagi.com/api/v0/search"
+    __version__ = "v0"
+    BASE_URL = f"https://kagi.com/api/{__version__}"
 
     def __init__(self, api_key: Optional[str] = None) -> None:
         if api_key is None:
             api_key = os.environ.get("KAGI_API_KEY")
 
         if api_key is None:
-            raise ValueError("No API key provided. Please provide one or set the KAGI_API_KEY environment variable.")
+            raise ValueError(
+                "No API key provided. Please provide one or set the KAGI_API_KEY environment variable."
+            )
 
         self.api_key = api_key
         self.session = requests.Session()
         self.session.headers.update({"Authorization": f"Bot {self.api_key}"})
 
-    def search(self, query: str, limit: int = 10):
+    def search(self, query: str, limit: int = 10) -> SearchResponse:
         params: Dict[str, Union[int, str]] = {"q": query, "limit": limit}
 
-        response = self.session.get(KagiClient.BASE_URL, params=params)
+        response = self.session.get(KagiClient.BASE_URL + "/search", params=params)
         response.raise_for_status()
-
-        return response.json()["data"]
+        print(response.status_code, response.content, response.url)
+        json_response = response.json()
+        return SearchResponse(meta=json_response["meta"], data=json_response["data"])
